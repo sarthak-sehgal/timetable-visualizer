a990b1f6cf37799f70f7f497fbad8e6e3f5ee592
Sarthak Sehgal <sarthaksehgal@Sarthaks-MacBook-Pro.local>
2019-01-02 18:06:53 +0530

    Fix check for days and hours

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let days = $("#days").select2('val');
         let hours = $("#hours").select2('val');
 
-        if(title && days && hours && section) {
+        if(title && days.length>0 && hours.length>0 && section) {
             let random = Math.floor(Math.random() * (BACKGROUNDS.length));
             while(BG_USED[random]) {
                 random = Math.floor(Math.random() * (BACKGROUNDS.length));
